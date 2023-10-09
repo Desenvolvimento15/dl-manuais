52db29724c743b91e2fb9f3bd48e5dfe7df3d26d
Your Name <96912707+Desenvolvimento15@users.noreply.github.com>
2023-10-09 14:38:13 -0300

    vs01

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+var buscando_resultados = false;
+$(document).ready(function(){
+	$("#filter").keyup(function(){
+		$('.lista_manuais p.txt_aguarde').show();
+		$('.lista_manuais div.cat').hide();
+		$('.lista_manuais p.nenhum_resultado').hide();
+		
+		if( buscando_resultados == false ){
+			buscando_resultados = true;
+			setTimeout(function(){
+				var filter_el = $('#filter');
+				var filter = $('#filter').val();
+				var contador = 0;
+				var contador_por_categoria = 0;
+				
+				$(".lista_manuais .cat").each(function(){
+					contador_por_categoria = 0;
+					$(this).find("li").each(function(){
+						if( $(this).html().search(new RegExp(filter, "i")) >= 0 ){
+							$(this).show();
+							$(this).siblings('h4.titulo_secao').show();
+							contador++;
+							contador_por_categoria++;
+						}
+						else{
+							$(this).hide();
+							if( $(this).is(':last-child') && contador_por_categoria == 0 ) $(this).siblings('h4.titulo_secao').hide();
+						}
+					});
+				});
+				
+				$('.lista_manuais p.txt_aguarde').hide();
+				
+				if( contador == 0 ){
+					$('.lista_manuais div.cat').hide();
+					$('.lista_manuais p.nenhum_resultado').show();
+				}
+				else {
+					$('.lista_manuais p.nenhum_resultado').hide();
+					$('.lista_manuais div.cat').show();
+				}
+				
+				buscando_resultados = false;
+			}, 1000);
+		}
+    });
+	
+	$(window).keydown(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			return false;
+		}
+	});
+});
