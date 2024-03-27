a780defcd340982f56401fbf578f6e89c5904f33
h3110 <desenvolvimento15@dl.com.br>
2024-03-27 08:04:33 -0300

    vs01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,23 @@
-var buscando_resultados = false;
-$(document).ready(function(){
-	$("#filter").keyup(function(){
-		$('.lista_manuais p.txt_aguarde').show();
-		$('.lista_manuais div.cat').hide();
-		$('.lista_manuais p.nenhum_resultado').hide();
-		
-		if( buscando_resultados == false ){
-			buscando_resultados = true;
-			setTimeout(function(){
-				var filter_el = $('#filter');
-				var filter = $('#filter').val();
-				var contador = 0;
-				var contador_por_categoria = 0;
-				
-				$(".lista_manuais .cat").each(function(){
-					contador_por_categoria = 0;
-					$(this).find("li").each(function(){
-						if( $(this).html().search(new RegExp(filter, "i")) >= 0 ){
-							$(this).show();
-							$(this).siblings('h4.titulo_secao').show();
-							contador++;
-							contador_por_categoria++;
-						}
-						else{
-							$(this).hide();
-							if( $(this).is(':last-child') && contador_por_categoria == 0 ) $(this).siblings('h4.titulo_secao').hide();
-						}
-					});
-				});
-				
-				$('.lista_manuais p.txt_aguarde').hide();
-				
-				if( contador == 0 ){
-					$('.lista_manuais div.cat').hide();
-					$('.lista_manuais p.nenhum_resultado').show();
-				}
-				else {
-					$('.lista_manuais p.nenhum_resultado').hide();
-					$('.lista_manuais div.cat').show();
-				}
-				
-				buscando_resultados = false;
-			}, 1000);
-		}
-    });
-	
-	$(window).keydown(function(event){
-		if(event.keyCode == 13) {
-			event.preventDefault();
-			return false;
-		}
-	});
+$("#filter").keypress(function(event) {
+    if (event.which == '13') {
+        event.preventDefault();
+        // Faz scroll na metade da tela quando a busca estiver completa
+        $('html, body').animate({
+            scrollTop: ($(window).height() / 2) + $('li.li-cat').first().position().top
+        }, 500);
+    }
+});
+
+$("li.li-cat").hide();
+
+$(".btn-categoria").click(function() {
+    var id_cat = $(this).attr("id");
+    // Esconde todos os itens da lista com a classe "li-cat"
+    $("li.li-cat").hide();
+    // Mostra itens da lista com a classe que coincide com o id do botão clicado
+    $("li." + id_cat).show();
+    // Faz scroll na metade da tela quando uma categoria for clicada
+    $('html, body').animate({
+        scrollTop: ($(window).height() / 2) + $('li.li-cat').first().position().top
+    }, 500);
 });
