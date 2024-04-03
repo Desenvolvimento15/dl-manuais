f8ce67f072c1e41f90f406e419e1dcfda938af52
h3110 <desenvolvimento15@dl.com.br>
2024-04-03 16:39:00 -0300

    vs01

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,27 @@ $("li.li-cat").hide();
 
 $("#filter").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $(".li-cat").hide().filter(function() {
-        return $(this).text().toLowerCase().indexOf(value) > -1;
-    }).show();
+    
+    // Exibir mensagem "Carregando..."
+    $(".loading").show();
+    $(".no-results").hide();
+
+    // Simular um pequeno atraso para demonstrar o estado "Carregando..."
+    setTimeout(function() {
+        var filteredItems = $(".li-cat").hide().filter(function() {
+            return $(this).text().toLowerCase().indexOf(value) > -1;
+        });
+
+        if (filteredItems.length === 0) {
+            $(".no-results").show();
+        } else {
+            $(".no-results").hide();
+            filteredItems.show();
+        }
+
+        // Ocultar mensagem "Carregando..."
+        $(".loading").hide();
+    }, 1000); // Altere este valor conforme necessário
 });
 
 $(".btn-categoria").click(function() {
